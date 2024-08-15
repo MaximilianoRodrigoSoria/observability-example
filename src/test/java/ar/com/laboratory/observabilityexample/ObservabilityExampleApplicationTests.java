67cee9a1e37e05536894fa5b0b9d946c9d9e9669
Maximiliano Rodrigo Soria <maximilianorodrigosoria@gmail.com>
2024-08-14 21:39:50 -0300

    Initial commit

---
+++ src/test/java/ar/com/laboratory/observabilityexample/ObservabilityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.laboratory.observabilityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
